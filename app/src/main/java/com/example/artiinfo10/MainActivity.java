84e088f2a82101b242609dae9ea49217a1cd97c8
otcz <54145021+otcz@users.noreply.github.com>
2021-05-30 14:10:32 -0500

    Commiti final
    Aqui todo el proyecto full para iniciar

--- app/src/main/java/com/example/artiinfo10/MainActivity.java
+++ app/src/main/java/com/example/artiinfo10/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.artiinfo10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnBuscar;
+    TextView texResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnBuscar = findViewById(R.id.btn_buscar);
+        // texResultado = findViewById(R.id.tex_resultado);
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("Lector -CDP");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(true);
+                integrator.setBarcodeImageEnabled(true);
+                integrator.initiateScan();
+                //   librosExcel librosExcel= new librosExcel();
+
+            }
+        });
+    }
+    protected void onActivityResult(int requesCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requesCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Lectura Cancelada", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                texResultado.setText(result.getContents());
+            }
+        } else {
+            super.onActivityResult(requesCode, resultCode, data);
+        }
+
     }
 }
